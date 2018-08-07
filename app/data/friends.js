80ee4c76b5038b2a431a98e276bcf01163b7564e
Phillip Katopodis <pkatopodis7@gmail.com>
2018-08-07 16:03:04 -0500

    latest

---
+++ app/data/friends.js
@@ -0,0 +1,55 @@
+// Application's Data of friends
+
+
+var friends = [
+    {
+    "name":"Ahmed",
+    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores":[5,1,4,4,5,1,2,5,4,1],
+    }, {
+    "name":"Joan",
+    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores":[2,2,5,3,1,5,2,2,1,3]
+  }, {
+    "name":"Alex",
+    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores":[1,2,2,1,5,2,4,1,2,5]
+  }, {
+    "name":"Haley",
+    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores":[5,2,4,1,2,5,3,1,2,2]
+  }, {
+    "name":"Rachel",
+    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores":[2,1,3,5,3,1,1,1,3,4]
+  }, {
+    "name":"Billy",
+    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores":[1,2,2,1,2,2,2,5,2,2]
+  }, {
+    "name":"Liz",
+    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores":[1,2,5,1,1,2,2,5,4,1]
+  }, {
+    "name":"Phill",
+    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores":[5,4,5,2,1,2,4,2,1,4]
+  }, {
+    "name":"Julie",
+    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores":[1,1,2,1,4,4,2,3,2,1]
+  }, {
+    "name":"Kristi",
+    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores":[3,3,5,2,3,1,1,4,2,5]
+  }
+];
+
+
+module.exports = friends;
+  
+ 
+
+  
+  
+  
